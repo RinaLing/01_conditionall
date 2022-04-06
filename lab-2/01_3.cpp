a290261ac19f0240809dcef669adba334bdfcec7
RinaLing <102519908+RinaLing@users.noreply.github.com>
2022-04-06 20:07:12 +0300

    Add files via upload

---
+++ lab-2/01_3.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    float n, a, b, x, y;//N, A, B, X, Y
+    cin >> n >> a >> b >> x >> y;
+    if (n < a && n < b) {
+        cout << n;
+    }
+    if (n >= a && n < b)
+    {
+        cout << n - (n * (x / 100));
+    }
+    if (n >= b)
+    {
+        cout << n - (n * (y / 100));
+    }
+    return 0;
+}
